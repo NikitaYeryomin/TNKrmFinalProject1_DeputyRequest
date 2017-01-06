1e218265a772fd7c74ffe00aa1578ed4f9143000
Alexey Poddubny <usatij.alex@gmail.com>
2017-01-06 23:23:49 +0000

    Подгрузка городов для регистрации

--- angular/controllers/city_controller.js
+++ angular/controllers/city_controller.js
@@ -7,7 +7,7 @@ app.controller('CityController', ['$scope', '$rootScope', '$http', '$location', 
         $scope.city = function() {
             $http({
                 method: 'GET',
-                url: '/backend/manage/city',
+                url: '/backend/city',
                 headers: {'Content-Type': 'application/x-www-form-urlencoded'}
             }).then(function(response) {
                 if (response.data.error == 0) {
