9753a59d14c0cbdfd7713d40dbe53644e8be8f25
Alexey Poddubny <usatij.alex@gmail.com>
2017-01-07 17:03:26 +0000

    Работа над регистрацией

--- angular/controllers/user_controller.js
+++ angular/controllers/user_controller.js
@@ -7,41 +7,30 @@ app.controller('UserController', ['$scope', '$http', '$location', '$state', 'Pag
         }
         
         $scope.register = function() {
-            if ($scope.Surname == '') {
-              //  
-            } else {
-                $http({
-                    method: 'POST',
-                    url: '/backend/user/register',
-                    data: $.param({
-                        'firstname' : $scope.Name,
-                        'secondname': $scope.Patronymic,
-                        'lastname'  : $scope.Surname,
-                        'email'     : $scope.Email,
-                        'phone'     : $scope.Phone,
-                        'password'  : $scope.Password,
-                        'city'      : $scope.City,
-                        'street'    : $scope.Street,
-                        'home'      : $scope.Home,
-                        'emaild'    : $scope.EmailDelivery
-                    }),
-                    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-                }).then(function(response) {
-                    console.log(response.data);
-                    if (response.data.error == 0) {
-                        $rootScope.logged_in = true;
-                        $rootScope.currentUser = response.data.currentUser;
-                        if ($rootScope.currentUser.role == 'admin') {
-                            console.log('redirecting to backend...');
-                            window.location.href = '/manage';
-                            //$location.path('/backend/manage/users')
-                        }
-                        else {
-                            $location.path('/districts');
-                        }
-                    }
-                });
-            }
+            $http({
+                method: 'POST',
+                url: '/backend/user/register',
+                data: $.param({
+                    'firstname' : $scope.Name,
+                    'secondname': $scope.Patronymic,
+                    'lastname'  : $scope.Surname,
+                    'email'     : $scope.Email,
+                    'phone'     : $scope.Phone,
+                    'password'  : $scope.Password,
+                    'city'      : $scope.City,
+                    'street'    : $scope.Street,
+                    'home'      : $scope.Home,
+                    'emaild'    : $scope.EmailDelivery
+                }),
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            }).then(function(response) {
+                console.log(response.data);
+                if (response.data.error == 0) {
+                    $rootScope.logged_in = true;
+                    $rootScope.currentUser = response.data.currentUser;
+                    $location.path('/districts');
+                }
+            });
         }
 
     }]);
