3d1e792d74fab736860788aa9c31b59ed3cc2fd0
Alexey Poddubny <usatij.alex@gmail.com>
2017-01-28 16:09:33 +0000

    Autocomplete directive in user_controller.js

--- angular/controllers/user_controller.js
+++ angular/controllers/user_controller.js
@@ -2,7 +2,7 @@ app.controller('UserController', ['$scope', '$rootScope', '$http', '$location', 
     function($scope, $rootScope, $http, $location, $state, $stateParams) {
         
         $scope.user = {};
-
+        
         $scope.register = function() {
             $http({
                 method: 'POST',
@@ -120,4 +120,56 @@ app.controller('UserController', ['$scope', '$rootScope', '$http', '$location', 
                 });
             }
         };
-    }]);
+    }])
+    .directive('ngAutocomplete', function($parse) {
+        return {
+            scope: {
+                details: '=',
+                ngAutocomplete: '=',
+                options: '='
+            },
+            link: function(scope, element, attrs, model) {
+                var opts;
+                var initOpts = function() {
+                    opts = {};
+                    if (scope.options) {
+                        if (scope.options.types) {
+                            opts.types = [];
+                            opts.types.push(scope.options.types);
+                        }
+                        if (scope.options.bounds) {
+                            opts.bounds = scope.options.bounds;
+                        }
+                        if (scope.options.country) {
+                            opts.componentRestrictions = {
+                                country: scope.options.country
+                            };
+                        }
+                    }
+                };
+                initOpts();
+                var newAutocomplete = function() {
+                    scope.gPlace = new google.maps.places.Autocomplete(element[0], opts);
+                    google.maps.event.addListener(scope.gPlace, 'place_changed', function() {
+                        scope.$apply(function() {
+                            //              if (scope.details) {
+                            scope.details = scope.gPlace.getPlace();
+                            //              }
+                            scope.ngAutocomplete = element.val();
+                        });
+                    });
+                };
+                newAutocomplete();
+                //watch options provided to directive
+                scope.watchOptions = function () {
+                    return scope.options;
+                };
+                scope.$watch(scope.watchOptions, function () {
+                    initOpts();
+                    newAutocomplete();
+                    element[0].value = '';
+                    scope.ngAutocomplete = element.val();
+                }, true);
+            }
+        };
+    });
