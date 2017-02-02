6500d2468cfdbe7ff951a4ffdc0b263d977c75e6
Felix <artemka_krm@ukr.net>
2017-02-02 20:14:29 +0000

    депутатики

--- angular/controllers/deputy_controller.js
+++ angular/controllers/deputy_controller.js
@@ -1,5 +1,9 @@
-app.controller('DeputyController', ['$scope', '$http', '$location', '$state', '$stateParams',
-   function($scope, $http, $location, $state, $stateParams) {$scope.id = $stateParams.id;}
+app.controller('DeputyController', ['$scope', '$http', '$location', '$state', '$stateParams',//'items',
+   function($scope, $http, $location, $state, $stateParams
+   //,items
+   ) {$scope.id = $stateParams.id;
+       //$scope.items = items.data;
+   }
     ])
 /******************************************список депутатов*************************************************/
 .directive('deputiesList', ['$http', function($http) {
