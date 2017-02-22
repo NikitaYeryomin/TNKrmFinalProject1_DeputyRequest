70d868f4e31479f67b87af58f7668d87990e3cad
Alexey Poddubny <usatij.alex@gmail.com>
2017-02-22 09:02:48 +0000

    Правильный автовыбор города в форме регистрации

--- angular/controllers/user_controller.js
+++ angular/controllers/user_controller.js
@@ -180,4 +180,18 @@ app.controller('UserController', ['$scope', '$rootScope', '$http', '$location', 
             }
         });
         
-    }]);
+    }])
+    
+.directive('convertToNumber', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attrs, ngModel) {
+            ngModel.$parsers.push(function(val) {
+                return val != null ? parseInt(val, 10) : null;
+            });
+            ngModel.$formatters.push(function(val) {
+                return val != null ? '' + val : null;
+            });
+        }
+    };
+});
